7eb31b0855d39067d6cc894ef53b678ca8ab8262
GuerrillaCoder <michael+github@digitalreach.online>
2022-01-03 08:08:19 +0000

    fix visibility

---
+++ src/Pipedrive.net/Models/Common/CustomFields/MultipleOptionCustomField.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Pipedrive.Models.Common.CustomFields
+{
+    public class MultipleOptionCustomField : List<int>, ICustomField
+    {
+        
+    }
+}
--- src/Pipedrive.net/Models/Common/Visibility.cs
+++ src/Pipedrive.net/Models/Common/Visibility.cs
@@ -2,7 +2,11 @@
 {
     public enum Visibility
     {
+        //https://devcommunity.pipedrive.com/t/new-field-lead-default-visibility-in-the-roles-api/4255
+        //changes depending on account level
         @private = 1,
-        shared = 3
+        shared = 3,
+        ownerAndBelow = 5,
+        entireCompany = 7
     }
 }
